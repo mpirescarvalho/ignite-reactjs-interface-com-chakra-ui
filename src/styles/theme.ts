7c0d1ae45e836ca4219c86a73d797f1ee5d835fc
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-09-01 11:42:37 -0300

    add colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,46 @@
 import { extendTheme, Theme } from '@chakra-ui/react';
 
+type MyTheme = {
+	colors: {
+		yellow: {
+			'500.50%': string;
+		};
+		gray: {
+			'400.50%': string;
+		};
+	};
+	styles: {
+		global: {
+			body: {
+				color: string;
+			};
+		};
+	};
+} & Theme;
+
 export const theme = extendTheme({
 	fonts: {
 		body: 'Poppins',
 		heading: 'Poppins',
 	},
-} as Theme);
+	colors: {
+		yellow: {
+			500: '#FFBA08',
+			'500.50%': '#FFBA0850',
+		},
+		gray: {
+			100: '#F5F8FA',
+			200: '#DADADA',
+			400: '#999999',
+			'400.50%': '#99999950',
+			700: '#47585B',
+		},
+	},
+	styles: {
+		global: {
+			body: {
+				color: 'gray.700',
+			},
+		},
+	},
+} as MyTheme);
